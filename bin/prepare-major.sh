e5fc6ed537852abf78f60e4c5381bb7a14e630ba
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-07-02 11:10:34 +0200

    Minor setup fixes

--- bin/prepare-major.sh
+++ bin/prepare-major.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd "$SCRIPT_DIR/.." || exit;
 # increment major version
 mvn build-helper:parse-version versions:set \
-      -DnewVersion =\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT \
+      -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0-SNAPSHOT \
       versions:commit

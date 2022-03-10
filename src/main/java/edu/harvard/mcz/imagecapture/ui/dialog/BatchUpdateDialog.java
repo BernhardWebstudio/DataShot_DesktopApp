85996d55778ad45186065c3331e8a4d1e55ad6e4
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-03-10 15:30:05 +0100

    Fix issue where batch update might fail for ollectors

--- src/main/java/edu/harvard/mcz/imagecapture/ui/dialog/BatchUpdateDialog.java
+++ src/main/java/edu/harvard/mcz/imagecapture/ui/dialog/BatchUpdateDialog.java
@@ -155,12 +155,12 @@ public class BatchUpdateDialog extends JDialog {
             Query updateQuery;
             // do the change
             if (fieldName.startsWith("s.")) {
-                updateQuery = session.createQuery("UPDATE Specimen s SET " + fieldName + "= :toValue, dateLastUpdated = NOW(), LastUpdatedBy = :updatingUser " + conditionQuery);
+                updateQuery = session.createQuery("UPDATE Specimen s SET " + fieldName + "= :toValue, dateLastUpdated = NOW(), lastUpdatedBy = :updatingUser " + conditionQuery);
                 updateQuery.setParameter("fromValue", getValueFromJTextField().getText());
             } else if (fieldName.startsWith("c.")) {
                 Query idsToUpdateQuery = session.createQuery("SELECT c.collectorId FROM Collector c LEFT JOIN c.specimen AS s " + conditionQuery);
                 idsToUpdateQuery.setParameter("fromValue", getValueFromJTextField().getText());
-                updateQuery = session.createQuery("UPDATE Collector c SET " + fieldName + " = :toValue, dateLastUpdated = NOW(), LastUpdatedBy = :updatingUser WHERE c.collectorId IN (:toUpdateIds)");
+                updateQuery = session.createQuery("UPDATE Collector c SET " + fieldName + " = :toValue WHERE c.collectorId IN (:toUpdateIds)");
                 updateQuery.setParameter("toUpdateIds", idsToUpdateQuery.getResultList());
             } else {
                 log.error("Unhandled updateable field prefix");

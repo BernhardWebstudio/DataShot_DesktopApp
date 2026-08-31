cae714044ca05c9711cc146f4fba9d8b7de5aedc
Tim Bernhard <tim@bernhard-webstudio.ch>
2026-08-31 19:53:38 +0200

    fix: merge Flyway ServiceLoader plugins in shaded jar and pass classloader

--- src/main/java/edu/harvard/mcz/imagecapture/lifecycle/AllowedVersionLifeCycle.java
+++ src/main/java/edu/harvard/mcz/imagecapture/lifecycle/AllowedVersionLifeCycle.java
@@ -54,7 +54,7 @@ public class AllowedVersionLifeCycle {
 		String url = properties.getProperty(ImageCaptureProperties.KEY_DB_URL);
 		String username = properties.getProperty(ImageCaptureProperties.KEY_DB_USER);
 		String password = properties.getProperty(ImageCaptureProperties.KEY_DB_PASSWORD);
-		return Flyway.configure().dataSource(url, username, password).baselineOnMigrate(true).baselineVersion("1.9.0")
+		return Flyway.configure(AllowedVersionLifeCycle.class.getClassLoader()).dataSource(url, username, password).baselineOnMigrate(true).baselineVersion("1.9.0")
 				.load();
 	}
 
